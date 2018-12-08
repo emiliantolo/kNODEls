2d883818bdaf5b20ec2a822e0c73ab59838029c3
emiliantolo <emiliantolo@gmail.com>
2018-12-08 19:30:46 +0100

    test cases (not complete)

--- api/controllers/tasksController.js
+++ api/controllers/tasksController.js
@@ -1,3 +1,4 @@
+const mongoose=require('mongoose')
 var TaskModel = require('./../models/taskModel.js')
 
 module.exports.getTasks = function (req,res){
@@ -22,9 +23,7 @@ module.exports.getTasks = function (req,res){
 	typeof(offset)==='number' &&
 	typeof(limit)==='number'
 
-    if(ok){
-	ok=ok && offset>=0 && limit>0
-    }
+    ok=ok && offset>=0 && limit>0
     
     console.log(offset+" "+limit)
     
@@ -51,7 +50,7 @@ module.exports.getTasks = function (req,res){
 
 	    })
 	    .catch(err => {
-		console.error(err)
+		//console.error(err)
 	    })
 		
 		}
@@ -66,30 +65,35 @@ module.exports.getTask = function (req,res){
     
     let taskid=req.params.TaskId;
     console.log(taskid)
-    
-    TaskModel
-	.findOne({_id: taskid})
-	.then(doc => {
-	    console.log(doc)
 
-	    if(doc===null){
-		//not found
-		res.status(404).send({error: "404"})
-	    }
-	    else{
-		//ok
-		res.contentType('application/json')	    
-		res.status(200)
-		res.json(doc)
-	    }
-	})
-	.catch(err => {
-	    console.error(err)
-	    //bad request
-	    res.status(400).send({error: "400"})
-	})
-	    
-	    };
+    if(mongoose.Types.ObjectId.isValid(taskid)){
+	
+	TaskModel
+	    .findOne({_id: taskid})
+	    .then(doc => {
+		console.log(doc)
+
+		if(doc===null){
+		    //not found
+		    res.status(404).send({error: "404"})
+		}
+		else{
+		    //ok
+		    res.contentType('application/json')	    
+		    res.status(200)
+		    res.json(doc)
+		}
+	    })
+	    .catch(err => {
+		//console.error(err)
+	    })
+		}
+    else{
+	//bad request
+	res.status(400).send({error: "400"})
+    }
+    
+};
 
 module.exports.createTask= function (req,res){
 
@@ -119,8 +123,8 @@ module.exports.createTask= function (req,res){
 	    Question: question,
 	    TaskType: taskType,
 	    TaskFile: taskFile,
-	    Options: options===undefined ? [] : options,
-	    Answers: answers===undefined ? [] : answers
+	    Options: !options ? [] : options,
+	    Answers: !answers ? [] : answers
 	})
 	
 	task.save()
@@ -132,10 +136,10 @@ module.exports.createTask= function (req,res){
 
 	    })
 	    .catch(err => {
-		console.error(err)
+		//console.error(err)
 	    })
 
-    }
+		}
     else{
 	//data invalid
 	res.status(400).send({error: "400"})
@@ -146,48 +150,99 @@ module.exports.updateTask= function (req,res){
 
     let taskid=req.params.TaskId;
     console.log(taskid)
-    
-    TaskModel
-	.findOne({_id: taskid})
-	.then(doc => {
-	    console.log(doc)
 
-	    if(doc===null){
-		//not found
-		res.status(404).send({error: "404"})
-	    }
-	    else{
-		//ok
+    if(mongoose.Types.ObjectId.isValid(taskid)){
 
-		let question=req.body.Question;
-		let taskType=req.body.TaskType;
-		let taskFile=req.body.TaskFile;
-		let options=req.body.Options;
-		let answers=req.body.Answers;
+	TaskModel
+	    .findOne({_id: taskid})
+	    .then(doc => {
+		console.log(doc)
 
-		let ok=true;
+		if(doc===null){
+		    //not found
+		    res.status(404).send({error: "404"})
+		}
+		else{
+		    //ok
 
-		ok= ok && (question===undefined || typeof(question)==='string')
-		ok= ok && (taskType===undefined || typeof(taskType)==='string')
-		ok= ok && (taskFile===undefined || typeof(taskFile)==='string')
-		ok= ok && (options===undefined || Array.isArray(options))
-		ok= ok && (answers===undefined || Array.isArray(answers))
+		    let question=req.body.Question;
+		    let taskType=req.body.TaskType;
+		    let taskFile=req.body.TaskFile;
+		    let options=req.body.Options;
+		    let answers=req.body.Answers;
 
-		if(ok){
+		    let ok=true;
+
+		    ok= ok && (!question || typeof(question)==='string')
+		    ok= ok && (!taskType || typeof(taskType)==='string')
+		    ok= ok && (!taskFile || typeof(taskFile)==='string')
+		    ok= ok && (!options || Array.isArray(options))
+		    ok= ok && (!answers || Array.isArray(answers))
+
+		    if(ok){
+			
+			let update={};
+
+			if(question){update.Question=question}
+			if(taskType){update.TaskType=taskType}
+			if(taskFile){update.TaskFile=taskFile}
+			if(options){update.Options=options}
+			if(answers){update.Answers=answers}
+			
+			
+			TaskModel
+			    .findOneAndUpdate({_id: taskid},
+					      update,
+					      {new: true})
+			    .then(doc => {
+				console.log(doc)
+				res.contentType('application/json')
+				res.status(200)
+				res.json(doc)
+			    })
+			    .catch(err => {
+				//console.error(err)
+			    })
+				}
+		    else{
+			//bad request
+			res.status(400).send({error: "400"})
+		    }
 		    
-		    let update={};
+		}
 
-		    if(question!==undefined){update.Question=question}
-		    if(taskType!==undefined){update.TaskType=taskType}
-		    if(taskFile!==undefined){update.TaskFile=taskFile}
-		    if(options!==undefined){update.Options=options}
-		    if(answers!==undefined){update.Answers=answers}
-		    
-		    
+	    })
+	    .catch(err => {
+		//console.error(err)
+	    })
+		}
+    else{
+	//bad request
+	res.status(400).send({error: "400"})
+    }
+
+};
+
+module.exports.deleteTask= function (req,res){
+
+    let taskid=req.params.TaskId;
+    console.log(taskid)
+    if(mongoose.Types.ObjectId.isValid(taskid)){
+	
+	TaskModel
+	    .findOne({_id: taskid})
+	    .then(doc => {
+		console.log(doc)
+
+		if(doc===null){
+		    //not found
+		    res.status(404).send({error: "404"})
+		}
+		else{
+		    //ok
+
 		    TaskModel
-			.findOneAndUpdate({_id: taskid},
-					  update,
-					  {new: true})
+			.findOneAndRemove({_id: taskid})
 			.then(doc => {
 			    console.log(doc)
 			    res.contentType('application/json')
@@ -195,60 +250,17 @@ module.exports.updateTask= function (req,res){
 			    res.json(doc)
 			})
 			.catch(err => {
-			    console.error(err)
+			    //console.error(err)
 			})
 			    }
-		else{
-		    console.error(err)
-		    //bad request
-		    res.status(400).send({error: "400"})		    
+	    })
+	    .catch(err => {
+		//console.error(err)
+	    })
 		}
-		
-	    }
-
-	})
-	.catch(err => {
-	    console.error(err)
-	    //bad request
-	    res.status(400).send({error: "400"})
-	})
-
-	    };
-
-module.exports.deleteTask= function (req,res){
-
-    let taskid=req.params.TaskId;
-    console.log(taskid)
-
-    TaskModel
-	.findOne({_id: taskid})
-	.then(doc => {
-	    console.log(doc)
-
-	    if(doc===null){
-		//not found
-		res.status(404).send({error: "404"})
-	    }
-	    else{
-		//ok
-
-		TaskModel
-		    .findOneAndRemove({_id: taskid})
-		    .then(doc => {
-			console.log(doc)
-			res.contentType('application/json')
-			res.status(200)
-			res.json(doc)
-		    })
-		    .catch(err => {
-			console.error(err)
-		    })
-			}
-	})
-	.catch(err => {
-	    console.error(err)
-	    //bad request
-	    res.status(400).send({error: "400"})
-	})
-	    
-	    };
+    else{
+	//bad request
+	res.status(400).send({error: "400"})
+    }
+    
+};
--- tests/tasks.test.js
+++ tests/tasks.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('./../app.js')
+var TaskModel = require('./../api/models/taskModel.js')
 
 /// root
 
@@ -12,36 +13,67 @@ describe('Test the root path', () => {
 })
 
 
-/// GET /v1/tasks
+/// GET /v1/tasks empty database
 
-describe('Test the /v1/tasks path no parameters', () => {
-    test('It should response the GET method', () => {
+describe('Test the /v1/tasks with GET empty database', () => {
+
+    beforeAll(function(done) {
+
+	TaskModel.remove({}).exec().then(() => {
+
+	    return done()
+	})
+    })
+
+    test('Normal path', () => {
 	return request(app).get("/v1/tasks").then(response => {
-	    expect(response.statusCode).toBe(200)
+	    expect(response.statusCode).toBe(404)
 	})
     });
-})
 
-describe('Test the /v1/tasks path wrong parameters', () => {
-    test('It should response the GET method', () => {
+    test('Negative parameters', () => {
 	return request(app).get("/v1/tasks?limit=-1&offset=-1").then(response => {
 	    expect(response.statusCode).toBe(400)
 	})
     });
-})
 
-describe('Test the /v1/tasks path off bound', () => {
-    test('It should response the GET method', () => {
+
+    test('Wrong offset parameter', () => {
+	return request(app).get("/v1/tasks?limit=10&offset=-1").then(response => {
+	    expect(response.statusCode).toBe(400)
+	})
+    });
+
+    test('Wrong limit parameter', () => {
+	return request(app).get("/v1/tasks?limit=0&offset=0").then(response => {
+	    expect(response.statusCode).toBe(400)
+	})
+    });
+    
+    test('Only offset parameter', () => {
+	return request(app).get("/v1/tasks?offset=0").then(response => {
+	    expect(response.statusCode).toBe(404)
+	})
+    });
+
+
+    test('Only limit parameter', () => {
+	return request(app).get("/v1/tasks?limit=5").then(response => {
+	    expect(response.statusCode).toBe(404)
+	})
+    });
+
+
+    test('Out of bounds', () => {
 	return request(app).get("/v1/tasks?limit=10&offset=1000").then(response => {
 	    expect(response.statusCode).toBe(404)
 	})
     });
-})
 
-describe('Test the /v1/tasks path right parameters', () => {
-    test('It should response the GET method', () => {
-	return request(app).get("/v1/tasks?limit=0&offset=5").then(response => {
-	    expect(response.statusCode).toBe(400)
+
+    test('Right parameters', () => {
+	return request(app).get("/v1/tasks?limit=5&offset=0").then(response => {
+	    expect(response.statusCode).toBe(404)
 	})
     });
 })
@@ -50,119 +82,585 @@ describe('Test the /v1/tasks path right parameters', () => {
 
 /// POST /v1/tasks
 
-describe('Test the /v1/tasks path create new right parameters', () => {
+describe('Test the /v1/tasks path with POST', () => {
+
+    beforeAll(function(done) {
+
+	TaskModel.remove({}).exec().then(() => {
+
+	    return done()
+	})
+    })
+    
     test('It should response the POST method', () => {
 	return request(app).post("/v1/tasks")
 	    .send({
-	"Question": "proident ut ad",
-	"TaskType": "dolore sit Lorem",
-	"TaskFile": "cillum sed quis"
+		"Question": "proident ut ad",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis"
 	    })
 	    .then(response => {
 		expect(response.statusCode).toBe(201)
-	})
+		expect(response.body.Question).toBe("proident ut ad")
+		expect(response.body.TaskType).toBe("dolore sit Lorem")
+		expect(response.body.TaskFile).toBe("cillum sed quis")		
+	    })
     });
-})
-
-describe('Test the /v1/tasks path create new wrong parameters', () => {
+    
     test('It should response the POST method', () => {
 	return request(app).post("/v1/tasks")
 	    .send({
-	"TaskType": "dolore sit Lorem",
-	"TaskFile": "cillum sed quis"
+		"Question": "proident ut ad",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis",
+		"Answers": "abcde"
 	    })
 	    .then(response => {
 		expect(response.statusCode).toBe(400)
-	})
+	    })
+    });
+    
+    test('It should response the POST method', () => {
+	return request(app).post("/v1/tasks")
+	    .send({
+		"Question": "",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis"
+	    })
+	    .then(response => {
+		expect(response.statusCode).toBe(400)
+	    })
+    });
+
+    test('It should response the POST method', () => {
+	return request(app).post("/v1/tasks")
+	    .send({
+		"Question": "proident ut ad",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis",
+		"Options": ["a", "b", "c"],
+		"Answers": ["a"]
+	    })
+	    .then(response => {
+		expect(response.statusCode).toBe(201)
+		expect(response.body.Question).toBe("proident ut ad")
+		expect(response.body.TaskType).toBe("dolore sit Lorem")
+		expect(response.body.TaskFile).toBe("cillum sed quis")	
+		expect(response.body.Options).toEqual(["a", "b", "c"])
+	    })
+    });
+    
+    test('Invalid Options', () => {
+	return request(app).post("/v1/tasks")
+	    .send({
+		"Question": "proident ut ad",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis",
+		"Options": "abc"
+	    })
+	    .then(response => {
+		expect(response.statusCode).toBe(400)
+	    })
+    });
+    
+    test('Invalid Answers', () => {
+	return request(app).post("/v1/tasks")
+	    .send({
+		"Question": "proident ut ad",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis",
+		"Answers": "abc"
+	    })
+	    .then(response => {
+		expect(response.statusCode).toBe(400)
+	    })
+    });
+    
+    test('It should response the POST method', () => {
+	return request(app).post("/v1/tasks")
+	    .send({
+		"Question": "proident ut ad",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis",
+		"Options": ["a", "b", "c"]
+	    })
+	    .then(response => {
+		expect(response.statusCode).toBe(201)
+		expect(response.body.Question).toBe("proident ut ad")
+		expect(response.body.TaskType).toBe("dolore sit Lorem")
+		expect(response.body.TaskFile).toBe("cillum sed quis")	
+		expect(response.body.Options).toEqual(["a", "b", "c"])
+	    })
     });
 })
 
 /// GET /v1/tasks/:TaskId
 
-describe('Test the /v1/tasks/:TaskId path get wrong id format', () => {
-    test('It should response the GET method', () => {
+describe('Test the /v1/tasks/:TaskId path with GET', () => {
+
+    beforeAll(function(done) {
+
+	TaskModel.remove({}).exec().then(() => {
+
+	    return done()
+	})
+    })
+    
+    test('Wrong id format', () => {
 	return request(app).get("/v1/tasks/abc")
 	    .then(response => {
 		expect(response.statusCode).toBe(400)
-	})
+	    })
     });
-})
 
-describe('Test the /v1/tasks/:TaskId path get right id format but not existing', () => {
-    test('It should response the GET method', () => {
+    test('Right id format', () => {
 	return request(app).get("/v1/tasks/5c057843d7e81322eb975b55")
 	    .then(response => {
 		expect(response.statusCode).toBe(404)
-	})
+	    })
     });
+    
 })
-
-describe('Test the /v1/tasks/:TaskId path get right id format and existing', () => {
-    test('It should response the GET method', () => {
-	return request(app).get("/v1/tasks/5c057843d7e81322eb975b55")
-	    .then(response => {
-		expect(response.statusCode).toBe(200)
-	})
-    });
-})
-
 /// PUT /v1/tasks/:TaskId
 
 describe('Test the /v1/tasks/:TaskId path get wrong id format', () => {
-    test('It should response the PUT method', () => {
+
+    beforeAll(function(done) {
+
+	TaskModel.remove({}).exec().then(() => {
+
+	    return done()
+	})
+    })
+    
+    test('Wrong id format', () => {
 	return request(app).put("/v1/tasks/abc")
 	    .then(response => {
 		expect(response.statusCode).toBe(400)
-	})
+	    })
     });
-})
 
-describe('Test the /v1/tasks/:TaskId path get right id format but not existing', () => {
-    test('It should response the PUT method', () => {
+    test('Right id format', () => {
 	return request(app).put("/v1/tasks/5c057843d7e81322eb975b55")
 	    .then(response => {
 		expect(response.statusCode).toBe(404)
-	})
+	    })
     });
-})
-
-describe('Test the /v1/tasks/:TaskId path get right id format and existing', () => {
-    test('It should response the PUT method', () => {
-	return request(app).put("/v1/tasks/5c057843d7e81322eb975b55")
-	    .then(response => {
-		expect(response.statusCode).toBe(200)
-	})
-    });
+    
 })
 /// DELETE /v1/tasks/:TaskId
 
 describe('Test the /v1/tasks/:TaskId path get wrong id format', () => {
-    test('It should response the DELETE method', () => {
-	return request(app).get("/v1/tasks/abc")
+
+    beforeAll(function(done) {
+
+	TaskModel.remove({}).exec().then(() => {
+
+	    return done()
+	})
+    })
+    
+    test('Wrong id format', () => {
+	return request(app).delete("/v1/tasks/abc")
 	    .then(response => {
 		expect(response.statusCode).toBe(400)
-	})
+	    })
     });
-})
 
-describe('Test the /v1/tasks/:TaskId path get right id format but not existing', () => {
-    test('It should response the DELETE method', () => {
-	return request(app).get("/v1/tasks/5c057843d7e81322eb975b55")
+    test('Right id format', () => {
+	return request(app).delete("/v1/tasks/5c057843d7e81322eb975b55")
 	    .then(response => {
 		expect(response.statusCode).toBe(404)
+	    })
+    });
+    
+})
+
+/// GET /v1/tasks/:TaskId with insertion
+
+describe('Test the /v1/tasks/:TaskId path with GET with data insert', () => {
+
+    beforeEach(function(done) {
+
+	TaskModel.remove({}).exec().then(() => {
+
+	    return done()
 	})
+    })
+    
+    test('Single id', () => {
+	return request(app).post("/v1/tasks")
+	    .send({
+		"Question": "proident ut ad",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis"
+	    })
+	    .then(postres => {
+		return (request(app).get("/v1/tasks/"+postres.body._id))
+		    .then((getres) => {
+			expect(getres.statusCode).toBe(200)
+			expect(getres.type).toBe("application/json")
+			expect(getres.body.Question).toBe("proident ut ad")
+			expect(getres.body.TaskType).toBe("dolore sit Lorem")
+			expect(getres.body.TaskFile).toBe("cillum sed quis")		
+		    })
+	    })
+
+    });
+
+    test('Multiple no offset, limit', () => {
+	return request(app).post("/v1/tasks")
+	    .send({
+		"Question": "proident ut ad",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis"})
+	    .then(res => {
+		return request(app).post("/v1/tasks")
+		    .send({
+			"Question": "proident ut ad",
+			"TaskType": "dolore sit Lorem",
+			"TaskFile": "cillum sed quis"})
+	    })
+	    .then(res => {
+		return request(app).post("/v1/tasks")
+		    .send({
+			"Question": "proident ut ad",
+			"TaskType": "dolore sit Lorem",
+			"TaskFile": "cillum sed quis"})
+	    })
+	    .then(res => {
+		return (request(app).get("/v1/tasks"))
+		    .then((getres) => {
+			expect(getres.statusCode).toBe(200)
+			expect(getres.type).toBe("application/json")
+			expect(getres.body.length).toBe(3)
+		    })
+	    })
+
+    });
+
+    test('Multiple with offset and limit', () => {
+	return request(app).post("/v1/tasks")
+	    .send({
+		"Question": "proident ut ad",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis"})
+	    .then(res => {
+		return request(app).post("/v1/tasks")
+		    .send({
+			"Question": "proident ut ad",
+			"TaskType": "dolore sit Lorem",
+			"TaskFile": "cillum sed quis"})
+	    })
+	    .then(res => {
+		return request(app).post("/v1/tasks")
+		    .send({
+			"Question": "proident ut ad",
+			"TaskType": "dolore sit Lorem",
+			"TaskFile": "cillum sed quis"})
+	    })
+	    .then(res => {
+		return (request(app).get("/v1/tasks?offset=0&limit=5"))
+		    .then((getres) => {
+			expect(getres.statusCode).toBe(200)
+			expect(getres.type).toBe("application/json")
+			expect(getres.body.length).toBe(3)
+		    })
+	    })
+
+    });
+    
+    test('Multiple with offset and limit limit output', () => {
+	return request(app).post("/v1/tasks")
+	    .send({
+		"Question": "proident ut ad",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis"})
+	    .then(res => {
+		return request(app).post("/v1/tasks")
+		    .send({
+			"Question": "proident ut ad",
+			"TaskType": "dolore sit Lorem",
+			"TaskFile": "cillum sed quis"})
+	    })
+	    .then(res => {
+		return request(app).post("/v1/tasks")
+		    .send({
+			"Question": "proident ut ad",
+			"TaskType": "dolore sit Lorem",
+			"TaskFile": "cillum sed quis"})
+	    })
+	    .then(res => {
+		return (request(app).get("/v1/tasks?offset=0&limit=1"))
+		    .then((getres) => {
+			expect(getres.statusCode).toBe(200)
+			expect(getres.type).toBe("application/json")
+			expect(getres.body.length).toBe(1)
+		    })
+	    })
+
+    });
+    
+    test('Multiple with offset offset output', () => {
+	return request(app).post("/v1/tasks")
+	    .send({
+		"Question": "proident ut ad",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis"})
+	    .then(res => {
+		return request(app).post("/v1/tasks")
+		    .send({
+			"Question": "proident ut ad",
+			"TaskType": "dolore sit Lorem",
+			"TaskFile": "cillum sed quis"})
+	    })
+	    .then(res => {
+		return request(app).post("/v1/tasks")
+		    .send({
+			"Question": "proident ut ad",
+			"TaskType": "dolore sit Lorem",
+			"TaskFile": "cillum sed quis"})
+	    })
+	    .then(res => {
+		return (request(app).get("/v1/tasks?offset=1&limit=5"))
+		    .then((getres) => {
+			expect(getres.statusCode).toBe(200)
+			expect(getres.type).toBe("application/json")
+			expect(getres.body.length).toBe(2)
+		    })
+	    })
+
+    });
+    
+    test('Multiple with only offset', () => {
+	return request(app).post("/v1/tasks")
+	    .send({
+		"Question": "proident ut ad",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis"})
+	    .then(res => {
+		return request(app).post("/v1/tasks")
+		    .send({
+			"Question": "proident ut ad",
+			"TaskType": "dolore sit Lorem",
+			"TaskFile": "cillum sed quis"})
+	    })
+	    .then(res => {
+		return request(app).post("/v1/tasks")
+		    .send({
+			"Question": "proident ut ad",
+			"TaskType": "dolore sit Lorem",
+			"TaskFile": "cillum sed quis"})
+	    })
+	    .then(res => {
+		return (request(app).get("/v1/tasks?offset=1"))
+		    .then((getres) => {
+			expect(getres.statusCode).toBe(200)
+			expect(getres.type).toBe("application/json")
+			expect(getres.body.length).toBe(2)
+		    })
+	    })
+
+    });
+    
+    test('Multiple with only limit', () => {
+	return request(app).post("/v1/tasks")
+	    .send({
+		"Question": "proident ut ad",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis"})
+	    .then(res => {
+		return request(app).post("/v1/tasks")
+		    .send({
+			"Question": "proident ut ad",
+			"TaskType": "dolore sit Lorem",
+			"TaskFile": "cillum sed quis"})
+	    })
+	    .then(res => {
+		return request(app).post("/v1/tasks")
+		    .send({
+			"Question": "proident ut ad",
+			"TaskType": "dolore sit Lorem",
+			"TaskFile": "cillum sed quis"})
+	    })
+	    .then(res => {
+		return (request(app).get("/v1/tasks?limit=2"))
+		    .then((getres) => {
+			expect(getres.statusCode).toBe(200)
+			expect(getres.type).toBe("application/json")
+			expect(getres.body.length).toBe(2)
+		    })
+	    })
+
+    });
+    
+    test('Multiple with high offset', () => {
+	return request(app).post("/v1/tasks")
+	    .send({
+		"Question": "proident ut ad",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis"})
+	    .then(res => {
+		return request(app).post("/v1/tasks")
+		    .send({
+			"Question": "proident ut ad",
+			"TaskType": "dolore sit Lorem",
+			"TaskFile": "cillum sed quis"})
+	    })
+	    .then(res => {
+		return request(app).post("/v1/tasks")
+		    .send({
+			"Question": "proident ut ad",
+			"TaskType": "dolore sit Lorem",
+			"TaskFile": "cillum sed quis"})
+	    })
+	    .then(res => {
+		return (request(app).get("/v1/tasks?offset=3"))
+		    .then((getres) => {
+			expect(getres.statusCode).toBe(404)
+		    })
+	    })
+
     });
 })
 
-describe('Test the /v1/tasks/:TaskId path get right id format and existing', () => {
-    test('It should response the DELETE method', () => {
-	return request(app).get("/v1/tasks/5c057843d7e81322eb975b55")
-	    .then(response => {
-		expect(response.statusCode).toBe(200)
+
+describe('Test the /v1/tasks/:TaskId path with PUT with data insert', () => {
+
+    beforeEach(function(done) {
+
+	TaskModel.remove({}).exec().then(() => {
+
+	    return done()
 	})
+    })
+    
+    test('Update Options and Answers', () => {
+	return request(app).post("/v1/tasks")
+	    .send({
+		"Question": "proident ut ad",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis"
+	    })
+	    .then(postres => {
+		return (request(app).put("/v1/tasks/"+postres.body._id).send({
+		    "Options": ["a", "b", "c"],
+		    "Answers": ["a"]
+	    }))
+		    .then((getres) => {
+			expect(getres.statusCode).toBe(200)
+			expect(getres.type).toBe("application/json")
+			expect(getres.body.Question).toBe("proident ut ad")
+			expect(getres.body.TaskType).toBe("dolore sit Lorem")
+			expect(getres.body.TaskFile).toBe("cillum sed quis")		
+			expect(getres.body.Options).toEqual(["a", "b", "c"])		
+			expect(getres.body.Answers).toEqual(["a"])		
+		    })
+	    })
+
+    });
+    
+    test('Invalid Options data', () => {
+	return request(app).post("/v1/tasks")
+	    .send({
+		"Question": "proident ut ad",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis"
+	    })
+	    .then(postres => {
+		return (request(app).put("/v1/tasks/"+postres.body._id).send({
+		    "Options": "abc"
+	    }))
+		    .then((getres) => {
+			expect(getres.statusCode).toBe(400)		
+		    })
+	    })
+
+    });
+    
+    test('Invalid Answers data', () => {
+	return request(app).post("/v1/tasks")
+	    .send({
+		"Question": "proident ut ad",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis"
+	    })
+	    .then(postres => {
+		return (request(app).put("/v1/tasks/"+postres.body._id).send({
+		    "Answers": "abc"
+	    }))
+		    .then((getres) => {
+			expect(getres.statusCode).toBe(400)		
+		    })
+	    })
+
+    });
+
+    
+    test('Modify Question, TaskType and TaskFile', () => {
+	return request(app).post("/v1/tasks")
+	    .send({
+		"Question": "proident ut ad",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis"
+	    })
+	    .then(postres => {
+		return (request(app).put("/v1/tasks/"+postres.body._id).send({
+		"Question": "q",
+		"TaskType": "tt",
+		"TaskFile": "tf"
+	    }))
+		    .then((getres) => {
+			expect(getres.statusCode).toBe(200)
+			expect(getres.body.Question).toBe("q")
+			expect(getres.body.TaskType).toBe("tt")
+			expect(getres.body.TaskFile).toBe("tf")
+		    })
+	    })
+
     });
 })
+
+describe('Test the /v1/tasks/:TaskId path with DELETE with data delete', () => {
+
+    beforeEach(function(done) {
+
+	TaskModel.remove({}).exec().then(() => {
+
+	    return done()
+	})
+    })
+    
+    test('Single id', () => {
+	return request(app).post("/v1/tasks")
+	    .send({
+		"Question": "proident ut ad",
+		"TaskType": "dolore sit Lorem",
+		"TaskFile": "cillum sed quis"
+	    })
+	    .then(postres => {
+		return (request(app).delete("/v1/tasks/"+postres.body._id))
+		    .then((getres) => {
+			expect(getres.statusCode).toBe(200)
+			expect(getres.type).toBe("application/json")
+			expect(getres.body.Question).toBe("proident ut ad")
+			expect(getres.body.TaskType).toBe("dolore sit Lorem")
+			expect(getres.body.TaskFile).toBe("cillum sed quis")	
+		    })
+	    })
+
+    });
+
+})
+/// GET /v1/tasks empty database
 /*
-expect(response.type).toEqual("application/json")
-expect(response.body.data).toEqual("Sending some JSON")
+  describe('Test the /v1/tasks path no parameters', () => {
+  test('It should response the GET method', () => {
+  return request(app).get("/v1/tasks").then(response => {
+  expect(response.statusCode).toBe(200)
+  expect(response.type).toBe("application/json")
+  expect(response.body).toEqual([])
+  })
+  });
+  })
 */
